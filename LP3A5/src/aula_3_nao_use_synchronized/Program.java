4681da70250bddd1fcbc5703f349efaaace14c7b
hochiminh1996 <ambrsp@hotmail.com>
2023-08-21 23:45:28 -0300

    Atualizando

--- LP3A5/src/aula_3_nao_use_synchronized/Program.java
+++ LP3A5/src/aula_3_nao_use_synchronized/Program.java
@@ -1,13 +1,31 @@
 package aula_3_nao_use_synchronized;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+/*
+ * Implementando threads em listas. (específico para listas)
+ * 
+ * A implementação em listas é um pouco diferente. Afinal, quando inserimos os valores, 
+ * o sistema diz que eles foram inseridos. No entanto, ao executar a thread mais de 3 vezes,
+ * nota-se que as vezes ela insere 1, 2 ou até 3. Nunca todas, justamente por causa da competição por recursos
+ * 
+ * 
+ * Collections.synchronized(lista);
+ * A inserção será feita corretamente com a subrescrita da lista padrão
+ * */
 
 public class Program {
 	private static List<String> lista = new ArrayList<>();
 	// uma lista de string
 
 	public static void main(String[] args) throws InterruptedException {
+		lista = Collections.synchronizedList(lista);
+		// sobrescreve nosso objeto lista e o definimos, via synchronizedList, que esse
+		// recurso será acessado exclusivamente uma vez por thread. Ou seja, limitando a
+		// competição e usando normalmente o conceito de paralelismo
+
 		MeuRunnable runnable = new MeuRunnable();
 
 		Thread t0 = new Thread(runnable);// thread 01
@@ -21,8 +39,8 @@ public class Program {
 		Thread.sleep(500);
 		// a thread atual ficará inativa por meio segundo. Isso dará tempo de executar
 		// todos os processos que estiverem no paralelismo
-		
-		System.out.println(lista);//exibe a lista
+
+		System.out.println(lista);// exibe a lista
 	}
 
 	// classe na main que implementa a interface runnable
@@ -31,10 +49,10 @@ public class Program {
 		@Override
 		public void run() {
 			String name = Thread.currentThread().getName();
-			//pegando o nome da thread
-			
+			// pegando o nome da thread
+
 			lista.add("Inserindo na lista: ");
-			System.out.println("Thread: "+name);
+			System.out.println("Thread: " + name);
 		}
 
 	}
